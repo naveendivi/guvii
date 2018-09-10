282156641dfe6d3d303cba36c619c88bc1944907
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-09-10 12:06:55 +0530

    Create program3.java

---
+++ player/program3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int f=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		while(n!=0)
+		{
+			int 	r=n%10;
+				f=r;
+				n=n/10;
+				System.out.print(f);
+		}
+		
+	}
+}
