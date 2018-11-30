ca9b7da97ceabe1921dc91186a42d934b847cda2
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-11-30 08:31:26 +0530

    Create program12.java

---
+++ player/program12.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc =new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		for(int i=0;i<n;i++)
+		{
+			int c=sc.nextInt();
+			al.add(c);
+		}
+		Collections.rotate(al,k);
+		for(int d:al)
+		{
+			System.out.println(d);
+		}
+		}
+		
+	}
