91a22286307cc271c6715ab8072d55c761c5caf9
naveendivi <41729252+naveendivi@users.noreply.github.com>
2018-11-30 09:04:54 +0530

    Update program13.java

--- player/program13.java
+++ player/program13.java
@@ -7,22 +7,15 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int k=1,p=0;
+		int v=0;		
 		Scanner sc =new Scanner(System.in);
 		int n=sc.nextInt();
-		int a[]=new int[n];
-		
-		for(int i=0;i<n;i++)
+		while(n!=0)
 		{
-			a[i]=sc.nextInt();
-			
+int			k=n%10;
+			v+=k*k;
+			n=n/10;
 		}
-		for(int j=0;j<n;j++)
-	{
-		k=a[j]*a[j];
-		p+=k;
-	}
-		
-		System.out.println(p);
+		System.out.println(v);
 	}
 }
